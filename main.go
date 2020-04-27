1fa808ef56dad1ef07a5f22efceb1b0c2e713dfc
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2020-04-27 12:18:36 +0300

    add type

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-func StringInSlice(str string, list []string) bool {
+type Utils struct {
+}
+
+func (u *Utils) stringInSlice(str string, list []string) bool {
 	for _, v := range list {
 		if v == str {
 			return true
